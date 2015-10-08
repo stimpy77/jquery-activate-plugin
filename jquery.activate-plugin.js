b14d80348176f73add603fae51b419f9f54cc0e4
Jon Davis <jon@jondavis.net>
2015-10-07 23:49:06 -0700

    re prev

--- jquery.activate-plugin.js
+++ jquery.activate-plugin.js
@@ -2,7 +2,7 @@
 // jon@jondavis.net
 // Activate a plugin that is normally activated in script using "$('#ID_TO_SOMETHING').myPlugin()"
 // on document ready by instead using <div data-jquery-activate="myPlugin">
-//                                                              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//                                         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 $(function() {
     $('*[data-jquery-activate]').each(function() {
         var plugins = $(this).data('jquery-activate').split(',');
