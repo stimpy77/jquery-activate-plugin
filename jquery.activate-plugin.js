c5734e6762c96aedc84c9cf183c236f6398f43ee
Jon Davis <jon@jondavis.net>
2015-10-07 23:46:29 -0700

    remove id from explanation

--- jquery.activate-plugin.js
+++ jquery.activate-plugin.js
@@ -1,7 +1,7 @@
 // jQuery Plugin Activate
 // jon@jondavis.net
 // Activate a plugin that is normally activated in script using "$('#ID_TO_SOMETHING').myPlugin()"
-// on document ready by instead using <div id="ID_TO_SOMETHING" data-jquery-activate="myPlugin">
+// on document ready by instead using <div data-jquery-activate="myPlugin">
 //                                                              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 $(function() {
     $('*[data-jquery-activate]').each(function() {
